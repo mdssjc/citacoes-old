8a213cb026eae4fd096138e2324581bba641a488
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-08 23:05:50 -0300

    Exceção para uso dos serviços DAO.

--- base/src/main/java/com/github/mdssjc/citacoes/dao/DaoException.java
+++ base/src/main/java/com/github/mdssjc/citacoes/dao/DaoException.java
@@ -1,26 +1,27 @@
 package com.github.mdssjc.citacoes.dao;
 
 /**
- * Exceção do Dao.
+ * Exceção do DAO.
  * 
  * @author Marcelo dos Santos
  *
  */
-@SuppressWarnings("serial")
 public class DaoException extends Exception {
+
+  private static final long serialVersionUID = 1L;
 
   public DaoException() {
   }
 
-  public DaoException(String message) {
+  public DaoException(final String message) {
     super(message);
   }
 
-  public DaoException(Throwable cause) {
+  public DaoException(final Throwable cause) {
     super(cause);
   }
 
-  public DaoException(String message, Throwable cause) {
+  public DaoException(final String message, final Throwable cause) {
     super(message, cause);
   }
 }
