9716f2fbec3517aac56bdd93c92f182bef42c083
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-10 18:47:00 -0300

    Estrutura para as etapas do cenário: Mensagem no Terminal.

---
+++ base/src/test/java/com/github/mdssjc/citacoes/base/MensagemNoTerminalStepsdef.java
@@ -0,0 +1,15 @@
+package com.github.mdssjc.citacoes.base;
+
+import cucumber.api.java8.En;
+
+/**
+ * Etapas do cenário: Mensagem no Terminal.
+ * 
+ * @author Marcelo dos Santos
+ *
+ */
+public class MensagemNoTerminalStepsdef implements En {
+
+  public MensagemNoTerminalStepsdef() {
+  }
+}
