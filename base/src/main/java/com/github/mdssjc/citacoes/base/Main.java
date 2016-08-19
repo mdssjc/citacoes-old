91f53a71d4146de8a4ac3be6e364b22d3037ed33
Marcelo dos Santos <mdssjc@gmail.com>
2016-08-19 19:43:36 -0300

    Implementação para impressão de mensagens.

--- base/src/main/java/com/github/mdssjc/citacoes/base/Main.java
+++ base/src/main/java/com/github/mdssjc/citacoes/base/Main.java
@@ -1,5 +1,8 @@
 package com.github.mdssjc.citacoes.base;
 
+import com.github.mdssjc.citacoes.dao.MensagemDao;
+import com.github.mdssjc.citacoes.model.Mensagem;
+
 /**
  * Classe de entrada da aplicação.
  * 
@@ -9,5 +12,14 @@ package com.github.mdssjc.citacoes.base;
 public class Main {
 
   public static void main(final String[] args) {
+    if (args.length == 0) {
+      final MensagemHandler handler = new MensagemHandler(new MensagemDao());
+      final Mensagem mensagem = handler.nextMessage();
+
+      System.out.println(mensagem.getId());
+      System.out.println(mensagem.getCategoria());
+      System.out.println(mensagem.getAutor());
+      System.out.println(mensagem.getTexto());
+    }
   }
 }
