2f02d3b2960dfa33b19b5d190ccfd7f96d5f43cf
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-25 00:41:39 -0200

    Refatoração.

--- base/src/main/java/com/github/mdssjc/citacoes/base/Main.java
+++ base/src/main/java/com/github/mdssjc/citacoes/base/Main.java
@@ -1,11 +1,8 @@
 package com.github.mdssjc.citacoes.base;
 
-import com.github.mdssjc.citacoes.dao.MensagemDao;
-import com.github.mdssjc.citacoes.model.Mensagem;
-
 /**
  * Classe de entrada da aplicação.
- * 
+ *
  * @author Marcelo dos Santos
  *
  */
@@ -13,13 +10,7 @@ public class Main {
 
   public static void main(final String[] args) {
     if (args.length == 0) {
-      final MensagemHandler handler = new MensagemHandler(new MensagemDao());
-      final Mensagem mensagem = handler.nextMessage();
-
-      System.out.println(mensagem.getId());
-      System.out.println(mensagem.getCategoria());
-      System.out.println(mensagem.getAutor());
-      System.out.println(mensagem.getTexto());
+      System.out.println(new MensagemHandler().nextMessage());
     }
   }
 }
