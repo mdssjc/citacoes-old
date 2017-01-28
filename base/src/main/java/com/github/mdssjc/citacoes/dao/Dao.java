1d5a0f243312f5719fcb0a66c52c475f4d22b4f5
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-28 00:41:14 -0200

    Refatoração da interface.

--- base/src/main/java/com/github/mdssjc/citacoes/dao/Dao.java
+++ base/src/main/java/com/github/mdssjc/citacoes/dao/Dao.java
@@ -4,47 +4,51 @@ import java.util.List;
 
 /**
  * Interface de abstração de dados (DAO).
- * 
+ *
  * @author Marcelo dos Santos
  *
  * @param <T>
- *          Tipo de dado
+ *     Tipo de dado
  */
 public interface Dao<T> {
 
   /**
    * Persiste o tipo no repositório.
-   * 
+   *
    * @param type
-   *          Tipo do dado
-   * @throws DaoException
+   *     Tipo do dado
    */
-  void save(T type) throws DaoException;
+  void save(T type);
 
   /**
    * Remove o tipo do repositório.
-   * 
+   *
    * @param type
-   *          Tipo do dado
-   * @throws DaoException
+   *     Tipo do dado
    */
-  void remove(T type) throws DaoException;
+  void remove(T type);
 
   /**
    * Seleciona o tipo através da identificação no repositório.
-   * 
+   *
    * @param id
-   *          Identificação do tipo
+   *     Identificação do tipo
+   *
    * @return Tipo recuperado
-   * @throws DaoException
    */
-  T find(long id) throws DaoException;
+  T find(long id);
 
   /**
    * Seleciona uma coleção dos tipos no repositório.
-   * 
+   *
    * @return Coleção dos tipos de dados
-   * @throws DaoException
    */
-  List<T> findAll() throws DaoException;
+  List<T> findAll();
+
+  /**
+   * Seleciona o tipo aleatóriamente no repositório.
+   *
+   * @return Tipo recuperado
+   */
+  T getRandom();
 }
