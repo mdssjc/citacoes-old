67e7374494d49f21ecaec9ccbfdb84c2551e140f
Marcelo dos Santos <mdssjc@gmail.com>
2018-03-08 14:19:44 -0300

    Entidade Quote.

--- java/src/main/java/com/github/mdssjc/citacoes/core/entities/Quote.java
+++ java/src/main/java/com/github/mdssjc/citacoes/core/entities/Quote.java
@@ -1,8 +1,26 @@
 package com.github.mdssjc.citacoes.core.entities;
 
+import lombok.Value;
+
 /**
+ * Representa a entidade Quote (Citação).
+ * <p>
+ * - define os elementos de uma citação: id, categoria, texto e autor.
+ *
  * @author Marcelo dos Santos
  */
+@Value
 public class Quote {
 
+  long id;
+  String categoria;
+  String texto;
+  String autor;
+
+  @Override
+  public String toString() {
+    return String.format("%d.%s: \"%s\" (%s)",
+                         this.id, this.categoria,
+                         this.texto, this.autor);
+  }
 }
